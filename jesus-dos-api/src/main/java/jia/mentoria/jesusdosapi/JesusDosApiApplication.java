a6f6b9e93b5baabdea6ca63f71a4948b51e48ef1
Jesus Fernandez <jesus.fernandez@contrastsecurity.com>
2021-03-19 14:17:38 -0600

    initial commit

---
+++ jesus-dos-api/src/main/java/jia/mentoria/jesusdosapi/JesusDosApiApplication.java
@@ -0,0 +1,13 @@
+package jia.mentoria.jesusdosapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JesusDosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JesusDosApiApplication.class, args);
+	}
+
+}
---
+++ jesus-dos-api/src/test/java/jia/mentoria/jesusdosapi/JesusDosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package jia.mentoria.jesusdosapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JesusDosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
